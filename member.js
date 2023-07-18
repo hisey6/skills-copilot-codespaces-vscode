404691a6fdfd51a5dd3e9e3d7221d46b2a2a26d4
hisey6 <45100684+hisey6@users.noreply.github.com>
2023-07-18 20:01:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS"];
+    var member = {
+        name: "A",
+        age: 20,
+        skills: skills
+    };
+    console.log(member.skills);
+    console.log(member.skills[1]);
+}
